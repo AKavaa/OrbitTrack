838d07beece9dcdd7efaed83d0193aa0fe1b2fbd
AKavaa <alex.kavaleuskiy33@gmail.com>
2026-03-14 13:35:27 +0200

    feat: Fetch NASA API and server responds with 26 satellites

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'dart:convert'; //converts JSON responses to text
+import 'package:http/http.dart' as http; // library for making HTTP requests
 
 void main() {
   runApp(const MyApp());
@@ -12,7 +14,9 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Orbit Track',
-      theme: ThemeData(colorScheme: .fromSeed(seedColor: Colors.deepPurple)),
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+      ),
       home: const MyHomePage(title: 'Orbit Track'),
     );
   }
@@ -28,6 +32,32 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  @override
+  void initState() {
+    super
+        .initState(); // this piece of code is always required, its initialising the parent class
+    fetchSatellites();
+  }
+
+  Future<void> fetchSatellites() async {
+    // async function to allown the program to start a long running task
+    final URL = Uri.parse('https://tle.ivanstanojevic.me/api/tle?search=ISS');
+
+    final response = await http.get(
+      URL,
+    ); // await -> pauses until the server responds
+
+    if (response.statusCode == 200) {
+      // request succesfull
+      final data = jsonDecode(
+        response.body,
+      ); // response.body by default is a raw JSON string, jsonDecode turns it into a Dart Map
+      print(response.body);
+    } else {
+      print('API ERROR - status code: ${response.statusCode}');
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
